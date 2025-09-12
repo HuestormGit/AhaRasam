40031b6fbe1bf3f21f2afaf50556510af0d0dcc8
divyag2196 <goruledivya2196@gmail.com>
2025-09-12 18:04:56 +0530

    cart update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -14,64 +14,86 @@ const Cart = () => {
     <div className="cart-container container-fluid p-0">
       <div className="navbg"></div>
       <div className="container">
-        <h2>Cart</h2>
-        {cart.length === 0 ? (
-          <p>Your cart is empty!</p>
-        ) : (
-          <>.
-            <table>
-              <tr className="tableheadingrow">
-                <th>Product</th>
-                <th>Weight</th>
-                <th>Amout</th>
-                <th>Quantity</th>
-              </tr>
-              {cart.map((item, idx) => ( 
-                <tr key={idx} className="tabledatarow">
-                  <td><h4>{item.productName}</h4></td>
-                  <td><p>{item.size}</p></td>
-                  <td><p> ₹{item.price * item.qty} </p></td>
-                  <td>
-                    <div className="d-flex align-items-center">
-                    <button className="trash-btn">🗑</button>
-                    <div className="varqty-sec">
-                      <button
-                        className="qty-btn qty-btn-left"
-                        // onClick={() => handleQtyChange(product.id, idx, -1)}
-                      >
-                        -
-                      </button>
-                      <span className="qty-value">
-                        {item.qty}
-                      </span>
-                      <button
-                        className="qty-btn qty-btn-right"
-                        // onClick={() => handleQtyChange(product.id, idx, 1)}
-                      >
-                        +
-                      </button>
-                    </div>
-                  </div>
-                  </td>
-                </tr>
-              ))}
+        <section className="cart-section">
+          <h2>Cart</h2>
+          {
+            cart.length === 0 ? (
+            <p>Your cart is empty!</p>
+            ) : (
+              <>
+                <div className="table-responsive">
+                  <table className="table">
+                    <thead class="red-dark">
+                      <tr className="tableheadingrow">
+                        <th>Product</th>
+                        <th>Weight</th>
+                        <th>Amout</th>
+                        <th>Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                  
+                      {cart.map((item, idx) => ( 
+                        <tr key={idx} className="tabledatarow">
+                          <td><h4>{item.productName}</h4></td>
+                          <td><p>{item.size}</p></td>
+                          <td><p> ₹{item.price * item.qty} </p></td>
+                          <td>
+                            <div className="d-flex align-items-center">
+                            <button className="trash-btn">🗑</button>
+                            <div className="varqty-sec">
+                              <button
+                                className="qty-btn qty-btn-left"
+                                // onClick={() => handleQtyChange(product.id, idx, -1)}
+                              >
+                                -
+                              </button>
+                              <span className="qty-value">
+                                {item.qty}
+                              </span>
+                              <button
+                                className="qty-btn qty-btn-right"
+                                // onClick={() => handleQtyChange(product.id, idx, 1)}
+                              >
+                                +
+                              </button>
+                            </div>
+                          </div>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th> <h3>Total </h3></th>
+                        <th colspan="2" class="text-end">₹{totalAmount}</th>
+                        <th>$35</th>
+                      </tr>
+                    </tfoot>
 
-            </table>
-            <ul>
-              {cart.map((item, idx) => (
-                <li key={idx}>
-                  <div>
-                    <strong>{item.productName}</strong> ({item.size}) - ₹
-                    {item.price} × {item.qty} = ₹{item.price * item.qty}
-                  </div>
-                  <button onClick={() => removeFromCart(idx)}>Remove</button>
-                </li>
-              ))}
-            </ul>
-            <h3>Total: ₹{totalAmount}</h3>
-            <button onClick={() => setShowCheckout(true)}>Proceed To Chekout</button>
-          </>
-        )}
+                  </table>
+                </div>
+                
+                {/* <ul>
+                  {cart.map((item, idx) => (
+                    <li key={idx}>
+                      <div>
+                        <strong>{item.productName}</strong> ({item.size}) - ₹
+                        {item.price} × {item.qty} = ₹{item.price * item.qty}
+                      </div>
+                      <button onClick={() => removeFromCart(idx)}>Remove</button>
+                    </li>
+                  ))}
+                </ul> */}
+                
+                <button onClick={() => setShowCheckout(true)}>Proceed To Chekout</button>
+              </>
+            )
+          }
+        </section>
+
+        
+        
 
         {/* {showCheckout && (
           <CheckoutPopup
