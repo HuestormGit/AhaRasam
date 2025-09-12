8f6c856691b81d12ad4ceda7b1f2324eba7031d6
divyag2196 <goruledivya2196@gmail.com>
2025-09-12 16:14:09 +0530

    css my header, video , contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -104,7 +104,7 @@ function Contact() {
         <div className="contact-form-box">
           <h2 className="form-title">Enquiry</h2>
           <p className="form-subtitle">
-            Have Questions or Want to Connect? We’d love to chat!
+            Have Questions or Want to Connect? <br></br>We’d love to chat!
           </p>
 
           <form className="contact-form" onSubmit={handleSubmit}>
