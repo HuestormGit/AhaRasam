9f87330c5b4bf8d26328825175d73124b2a2af0a
divyag2196 <goruledivya2196@gmail.com>
2025-09-12 14:53:03 +0530

    css match videosection and contact page

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -9,7 +9,7 @@ function VideoSection() {
       <div className="container">
         <div className="row ">
           {/* align-items-center */}
-          <div className="col-lg-5 col-md-12 text-center">
+          <div className="col-lg-5 col-md-12 text-center videoimg">
               {/* <iframe width="337" height="599" src="https://www.youtube.com/embed/Dj1wkE9GBUg" title="How to make a perfect Rasam | AHA! Rasam" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen ></iframe> */}
               <a href="https://www.youtube.com/watch?v=Dj1wkE9GBUg" target="_blank">
                 <img src={videobgimg} alt="Watch Rasam Video" width="337" height="599" className="VideoSectionimg" />
