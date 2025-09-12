77d857dacf46628ddd0a41d8524488bb78379c44
divyag2196 <goruledivya2196@gmail.com>
2025-09-12 10:10:32 +0530

    css update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -104,7 +104,7 @@ function Contact() {
         <div className="contact-form-box">
           <h2 className="form-title">Enquiry</h2>
           <p className="form-subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Have Questions or Want to Connect? We’d love to chat!
           </p>
 
           <form className="contact-form" onSubmit={handleSubmit}>
