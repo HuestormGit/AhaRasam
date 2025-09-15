177c2d01a8d3ca964f28d70f878111d85d3c87fc
divyag2196 <goruledivya2196@gmail.com>
2025-09-15 15:41:32 +0530

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 import "./Cart.scss";
-import Checkout from "../Checkout/Checkout";
+import Checkout from "../Checkout/Checkout";.
+import { trash } from "../../assets/trash.png";
 
 const Cart = () => {
   const { cart, setCart } = useContext(CartContext); // ✅ make sure CartContext provides setCart
