281c1556d3856d745a1e9903576a064cca9854ca
divyag2196 <goruledivya2196@gmail.com>
2025-09-15 15:46:22 +0530

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 import "./Cart.scss";
-import Checkout from "../Checkout/Checkout";.
+import Checkout from "../Checkout/Checkout";
 import { trash } from "../../assets/trash.png";
 
 const Cart = () => {
