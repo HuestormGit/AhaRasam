edab7627dbacc4de61df91c3a1ea2e8927cd4e8a
divyag2196 <goruledivya2196@gmail.com>
2025-09-16 17:24:30 +0530

    update banner

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,13 +1,13 @@
 import "./Banner.scss";
 import bannerImg from  "../../../assets/banner-image.png";
-import sec11 from  "../../../assets/sec11.png";
-import sec12 from  "../../../assets/sec12.png";
-import sec13 from  "../../../assets/sec13.png";
-// import leftpoint from "../../../assets/Leftpoint.png"
-// import Rightpoints from "../../../assets/Rightpoints.png"
-import sec31 from  "../../../assets/sec11.png";
-import sec32 from  "../../../assets/sec12.png";
-import sec33 from  "../../../assets/sec13.png";
+// import sec11 from  "../../../assets/sec11.png";
+// import sec12 from  "../../../assets/sec12.png";
+// import sec13 from  "../../../assets/sec13.png";
+import leftpoint from "../../../assets/Leftpoint.png"
+import Rightpoints from "../../../assets/Rightpoints.png"
+// import sec31 from  "../../../assets/sec11.png";
+// import sec32 from  "../../../assets/sec12.png";
+// import sec33 from  "../../../assets/sec13.png";
 import { useEffect, useState } from "react";
 
 
@@ -37,27 +37,29 @@ const Banner = () => {
         </div>
         <div className="banner-box2 container">
             <div className="sec-1">
-  <img src={sec11} className="sec11img" alt="Heart" />
-  <img src={sec12} className="sec12img" alt="Digestive" />
-  <img src={sec13} className="sec13img" alt="Improves" />
-</div>
+                <img src={leftpoint} className="sec11img" alt="Heart" />
+                {/* <img src={sec11} className="sec11img" alt="Heart" />
+                <img src={sec12} className="sec12img" alt="Digestive" />
+                <img src={sec13} className="sec13img" alt="Improves" /> */}
+            </div>
 
-<div className="sec-2">
-  <img
-    src={bannerImg}
-    alt="Rasam Bowl"
-    style={{
-      transform: `rotate(${rotation}deg)`,
-      transition: "transform 0.1s linear"
-    }}
-  />
-</div>
+            <div className="sec-2">
+            <img
+                src={bannerImg}
+                alt="Rasam Bowl"
+                style={{
+                transform: `rotate(${rotation}deg)`,
+                transition: "transform 0.1s linear"
+                }}
+            />
+            </div>
 
-<div className="sec-3">
-  <img src={sec31} className="sec31img" alt="Boosts Immunity" />
-  <img src={sec32} className="sec32img" alt="Rich in Antioxidants" />
-  <img src={sec33} className="sec33img" alt="Other" />
-</div>
+            <div className="sec-3">
+                <img src={Rightpoints} className="sec31img" alt="Boosts Immunity" />
+                {/* <img src={sec31} className="sec31img" alt="Boosts Immunity" />
+                <img src={sec32} className="sec32img" alt="Rich in Antioxidants" />
+                <img src={sec33} className="sec33img" alt="Other" /> */}
+            </div>
 
         </div>
 
