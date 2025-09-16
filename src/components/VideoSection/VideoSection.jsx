cc1e7bbffc26c1cb9af9f2b2d1d94e28ac96a95c
divyag2196 <goruledivya2196@gmail.com>
2025-09-16 13:10:42 +0530

    videosection content replace css update
    content replace

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -9,18 +9,18 @@ function VideoSection() {
       <div className="container">
         <div className="row ">
           {/* align-items-center */}
-          <div className="col-lg-7 col-md-6 col-sm-12 text-center videoimg">
+          <div className="col-lg-6 col-md-6 col-sm-12 text-center videoimg">
               {/* <iframe width="337" height="599" src="https://www.youtube.com/embed/Dj1wkE9GBUg" title="How to make a perfect Rasam | AHA! Rasam" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen ></iframe> */}
               <a href="https://www.youtube.com/watch?v=Dj1wkE9GBUg" target="_blank">
                 <img src={videobgimg} alt="Watch Rasam Video" className="VideoSectionimg" />
               </a>
 
           </div>
-          <div className="col-lg-5 col-md-6 col-sm-12">
+          <div className="col-lg-6 col-md-6 col-sm-12">
             <div className="spacer"></div>
-            <h3 className="videosectionheading">How to make a perfect Rasam</h3>
+            <h3 className="videosectionheading">Make the Perfect Rasam!</h3>
             <p className="videosectionpara">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dictum, mi non maximus accumsan, tortor nunc laoreet dolor, ac ullamcorper massa lacus vel felis.
+              Treat your family and friends to this authentic Rasam recipe.<br></br> Simple to make, bursting with flavor, and truly divine in taste! Let’s cook!
             </p>
           </div>
         </div>
