ade46ceda64ac0378b33957c05493b22cc7d1a2e
divyag2196 <goruledivya2196@gmail.com>
2025-11-07 15:48:52 +0530

    about

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -11,17 +11,20 @@ function AboutUs() {
                 <div className="row ">
                     <div className="col-6">
                         <p className='text-start'>
-                            For us, rasam is more than a beverage, it’s a heritage passed down through generations, a perfect balance of flavor, nourishment, and care that has long been a part of Indian kitchens.
+                            For us, rasam is more than a beverage, it’s a heritage passed down <br></br> through generations, a perfect balance of flavor, nourishment, and <br></br> care that has long been a part of Indian kitchens.
                         </p>
                     </div>
                     <div className="col-6">
                         <p className='text-start'>
-                           We honor this tradition by crafting each cup with authentic ingredients like toor dal, tamarind, pepper, and curry leaves, preserving its wholesome essence while making it accessible for modern life. We celebrate this heritage by keeping its spirit alive in every cup, connecting people to the simple joys and mindful living of the past.
+                           We honor this tradition by crafting each cup with authentic <br></br> ingredients like toor dal, tamarind, pepper, and curry leaves, <br></br> preserving its wholesome essence while making it accessible for <br></br> modern life. We celebrate this heritage by keeping its spirit alive in <br></br> every cup, connecting people to the simple joys and mindful living <br></br> of the past.
                         </p>
                     </div>
                 </div>
-                <h5>
-                    And because we believe that goodness is meant to be shared, every sip contributes to social impact, supporting education, healthcare, temple preservation, Gau Samrakshana, and the welfare of ex-army families, turning a simple daily ritual into a gesture of care for the wider community.
+                <h5 className='subheading'>
+                    And because we believe that goodness is meant to be shared, every <br></br> 
+                    sip contributes to social impact, supporting education, healthcare,<br></br> 
+                    temple preservation, Gau Samrakshana, and the welfare of ex-army <br></br> 
+                    families, turning a simple daily ritual into a gesture of care for the<br></br>  wider community.
                 </h5>
             </div>
         </div>
