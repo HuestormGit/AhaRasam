e99c49d4d348f3bb6f1ff62091f31607e6227b1e
divyag2196 <goruledivya2196@gmail.com>
2025-11-07 11:56:49 +0530

    aboutus changes

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -5,24 +5,24 @@ function AboutUs() {
     <section className="AboutUs" id="AboutUs">
         <div className="container-fluid">
             <div className="container text-center">
-                <h2>At Aha! Rasam, we believe life’s best comforts 
+                <h2 className='AboutUsHeading'>At Aha! Rasam, we believe life’s best comforts 
                     <br></br>don’t need invention, only rediscovery.
                 </h2>
                 <div className="row ">
                     <div className="col-6">
-                        <p>
+                        <p className='text-left'>
                             For us, rasam is more than a beverage, it’s a heritage passed down through generations, a perfect balance of flavor, nourishment, and care that has long been a part of Indian kitchens.
                         </p>
                     </div>
                     <div className="col-6">
-                        <p>
+                        <p className='text-left'>
                            We honor this tradition by crafting each cup with authentic ingredients like toor dal, tamarind, pepper, and curry leaves, preserving its wholesome essence while making it accessible for modern life. We celebrate this heritage by keeping its spirit alive in every cup, connecting people to the simple joys and mindful living of the past.
                         </p>
                     </div>
                 </div>
-                <p>
+                <h5>
                     And because we believe that goodness is meant to be shared, every sip contributes to social impact, supporting education, healthcare, temple preservation, Gau Samrakshana, and the welfare of ex-army families, turning a simple daily ritual into a gesture of care for the wider community.
-                </p>
+                </h5>
             </div>
         </div>
     </section>
