d0cfcb1220c2e62536faab988c1b39479a3a722e
divyag2196 <goruledivya2196@gmail.com>
2025-11-19 17:33:00 +0530

    about us contact us css

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -6,28 +6,25 @@ function AboutUs() {
         <div className="container-fluid">
             <div className="container text-center">
                 <h2 className='AboutUsHeading'>At Aha! Rasam, we believe life’s best comforts 
-                    <br></br>don’t need invention, only rediscovery.
+                    don’t need invention, only rediscovery.
                 </h2>
                 <div className="row pt-50">
                     <div className="col-lg-1 col-md-1 col-sm-12"></div>
                     <div className="col-lg-5 col-md-5 col-sm-12">
-                        <p className='text-start'>
+                        <p>
                             For us, rasam is more than a beverage, it’s a heritage passed down <br></br> through generations, a perfect balance of flavor, nourishment, and <br></br> care that has long been a part of Indian kitchens.
                         </p>
                     </div>
                     <div className="col-lg-5 col-md-5 col-sm-12">
-                        <p className='text-start'>
+                        <p>
                            We honor this tradition by crafting each cup with authentic <br></br> ingredients like toor dal, tamarind, pepper, and curry leaves, <br></br> preserving its wholesome essence while making it accessible for <br></br> modern life. We celebrate this heritage by keeping its spirit alive in <br></br> every cup, connecting people to the simple joys and mindful living <br></br> of the past.
                         </p>
                     </div>
                     <div className="col-lg-1 col-md-1 col-sm-12"></div>
 
                 </div>
-                <h5 className='subheading text-center'>
-                    And because we believe that goodness is meant to be shared, every <br></br> 
-                    sip contributes to social impact, supporting education, healthcare,<br></br> 
-                    temple preservation, Gau Samrakshana, and the welfare of ex-army <br></br> 
-                    families, turning a simple daily ritual into a gesture of care for the<br></br>  wider community.
+                <h5>
+                    And because we believe that goodness is meant to be shared, every sip contributes to social impact, supporting education, healthcare, temple preservation, Gau Samrakshana, and the welfare of ex-army families, turning a simple daily ritual into a gesture of care for the wider community.               
                 </h5>
             </div>
         </div>
