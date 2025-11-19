a6eba3c7a47197c517447747c74bf4b91962d014
divyag2196 <goruledivya2196@gmail.com>
2025-11-19 18:04:58 +0530

    videosection css update

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -7,7 +7,7 @@ function VideoSection() {
      <section className="VideoSection" id="VideoSection">
       <div className="container-fluid">
       <div className="container">
-        <div className="row ">
+        <div className="row videopart">
           {/* align-items-center */}
           <div className="col-lg-6 col-md-6 col-sm-12 text-center videoimg">
               {/* <iframe width="337" height="599" src="https://www.youtube.com/embed/Dj1wkE9GBUg" title="How to make a perfect Rasam | AHA! Rasam" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen ></iframe> */}
@@ -16,7 +16,7 @@ function VideoSection() {
               </a>
 
           </div>
-          <div className="col-lg-6 col-md-6 col-sm-12">
+          <div className="col-lg-6 col-md-6 col-sm-12 videoinfo">
             <div className="spacer"></div>
             <h3 className="videosectionheading">Make the Perfect Rasam!</h3>
             <p className="videosectionpara">
