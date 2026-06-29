9aa3a2f6f3500744a8c15cf5dcefa6094c1a4334
HuestormGit <sameer@huestorm.com>
2026-06-29 13:23:29 +0530

    content replace as per mail
    banner section and video section

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -32,7 +32,7 @@ const Banner = () => {
                     A Sip of Tradition, <br></br>A Taste of Wellness
                 </h1>
                 <h6 className="sub-heading">
-                    A healthy replacement for Tea or Coffee
+                    Sip it as a healthy, comforting Rasam, <br/>or sprinkle it to elevate your daily cooking!
                 </h6>
             </div>
 
